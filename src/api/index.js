8e8287bd1dadebaf9ea064ea7d61d0238407df1f
Vladyslav Dobychin <vladyslavdobychin@gmail.com>
2023-06-12 15:06:15 +0300

    Add axios to the project
    Replaced all fetch() functions with axios.get() for improved error handling.
    
    :wq#

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,23 @@
-export const getCurrentWeather = async (location) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=021044969dbd22e777fb1835b345e37f&units=metric`
-  );
+import axios from 'axios';
 
-  if (!response.ok) {
+export const getCurrentWeather = async (location) => {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=021044969dbd22e777fb1835b345e37f&units=metric`
+    );
+    return response.data;
+  } catch (error) {
     throw new Error('City not found');
   }
-
-  const data = await response.json();
-  return data;
 };
 
 export const getWeatherForecast = async (location) => {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=021044969dbd22e777fb1835b345e37f&units=metric`
-  );
-
-  if (!response.ok) {
+  try {
+    const response = await axios.get(
+      `https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=021044969dbd22e777fb1835b345e37f&units=metric`
+    );
+    return response.data;
+  } catch (error) { 
     throw new Error('City not found');
   }
-
-  const data = await response.json();
-  return data;
 };
